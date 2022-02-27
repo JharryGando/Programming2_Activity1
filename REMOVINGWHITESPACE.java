2691becefb1925b989857466757d43e596753bbf
JohnHarryGando <100146366+JharryGando@users.noreply.github.com>
2022-02-27 18:03:57 +0800

    Add files via upload

---
+++ REMOVINGWHITESPACE.java
@@ -0,0 +1,13 @@
+package removingspace.java;
+
+public class REMOVINGWHITESPACE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			   String str = "I Love You So Much";
+			   str = str.replaceAll(" ", "");
+			   System.out.println(str);
+				}
+           }
